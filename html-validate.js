a2613dce27b0c8bc9125efae198296ffe4318cd8
Owen Rogers <exortech@gmail.com>
2017-03-16 14:57:16 -0700

    update html-validate script

--- html-validate.js
+++ html-validate.js
@@ -1,46 +1,45 @@
-const fs = require('fs-extra');
-const validator = require('html-validator');
+const fs = require('fs-extra')
+const path = require('path')
+const validator = require('html-validator')
 
-// var files = [
-// 	'./public/index.html',
-// 	'./public/champions/index.html',
-// 	'./public/contact/index.html',
-// 	'./public/faq/index.html',
-// ];
+const skipFiles = [
+// 'public/registrar/index.html'
+]
 
-var skip = [
-	'Attribute “color” not allowed on element “link” at this point.'
-];
+const skip = [
+  'Attribute “color” not allowed on element “link” at this point.',
+  'Illegal character in query: “|” is not allowed.'
+]
 
 const validate = function (file) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(file, (err, data) => err ? reject(err) : resolve(data));
-	}).then((data) => {
-		let options = {
-			data: data,
-			format: 'json'
-		};
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => err ? reject(err) : resolve(data))
+  }).then((data) => {
+    let options = {
+      data: data,
+      format: 'json'
+    }
 
-		return new Promise((resolve, reject) => {
-			validator(options, (err, data) => err ? reject(err) : resolve(data));
-		});
-	}).then((results) => {
-		console.log('Validating', file);
-		// console.log(data);
-		results.messages.filter((entry) => {
-			return ! skip.includes(entry.message);
-		}).map((entry) => {
-			console.log(`${entry.type.toUpperCase()}: ${entry.message} (line: ${entry.lastLine})`);
-		});
-		console.log();
-	});
-};
+    return new Promise((resolve, reject) => {
+      validator(options, (err, data) => err ? reject(err) : resolve(data))
+    })
+  }).then((results) => {
+    console.log('Validating', file)
+    results.messages.filter((entry) => {
+      return !skip.some(s => entry.message.includes(s))
+    }).map((entry) => {
+      console.log(`${entry.type.toUpperCase()}: ${entry.message} (line: ${entry.lastLine})`)
+    })
+    console.log()
+  })
+}
 
 new Promise((resolve, reject) => {
-	let files = [];
-	fs.walk('./public')
-		.on('data', (file) => file.path.endsWith('.html') ? files.push(file.path) : null)
-		.on('end', () => resolve(files));
+  let files = []
+  fs.walk('./public').on('data', (file) => {
+    let skipFile = skipFiles.some(s => s === path.relative(process.cwd(), file.path))
+    return file.path.endsWith('.html') && !skipFile ? files.push(file.path) : null
+  }).on('end', () => resolve(files))
 }).then((files) => {
-	Promise.all(files.map(validate))
-});
+  Promise.all(files.map(validate))
+})
